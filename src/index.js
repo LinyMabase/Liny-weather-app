fad2d59432337459b5bcffdcbc514e9fa1436d26
LinyMabase <leinymagabane@gmail.com>
2024-10-13 05:07:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+function searchCity(city) {
+  let apiKey = "83e0a0o37b76946tb2f37479124d5029";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=matric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
